9f36a0a8d8e79d2e50b38219e8b7998d01dda465
BradlyeG <gordonbradlye@gmail.com>
2025-02-21 01:54:20 -0600

    Update mqtt_sign_updater.py
    Cleaned up style to handle pylint warnings

--- mqtt_sign_updater.py
+++ mqtt_sign_updater.py
@@ -1,32 +1,50 @@
-from paho.mqtt import client as mqtt_client
 import time
+from paho.mqtt import client as mqtt_client
 
 # Configuration options
-broker = "broker_address"
-port = 1883
-client_id = "Python Sign Updater"
-username = "username"
-password = "password"
-topic = "your-sign-topic"
+BROKER = "broker_address"
+PORT = 1883
+CLIENT_ID = "Python Sign Updater"
+USERNAME = "username"
+PASSWORD = "password"
+TOPIC = "your-sign-topic"
 
 def connect_mqtt():
+    """
+    Function to handle connecting to broker and associated helper functions.
+    
+    Args:
+        None
+
+    Returns:
+        None
+    """
     def on_connect(client, userdata, flags, rc, properties):
         if rc == 0:
             print("\nConnected to MQTT Broker!")
         else:
-            print("\nFailed to connect, return code %d\n", rc)       
+            print("\nFailed to connect, return code %d\n", rc)
 
     # Set client ID
-    client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
+    client = mqtt_client.Client(client_id=CLIENT_ID, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
 
     # Set uname/pass and then connect
-    client.username_pw_set(username, password)
+    client.username_pw_set(USERNAME, PASSWORD)
     #client.tls_set('./certs/ca.crt')
     client.on_connect = on_connect
-    client.connect(broker, port)
+    client.connect(BROKER, PORT)
     return client
 
 def run():
+    """
+    Main Runner Function
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
     print("\t****************************************")
     print("\t***         MQTT Sign Updater        ***")
     print("\t****************************************")
@@ -37,9 +55,10 @@ def run():
     while not client.is_connected():
         time.sleep(0.1)
     message = input("\nPlease enter brief message to display on sign: ")
-    client.publish(topic, message, qos=1)
+    client.publish(TOPIC, message, qos=1)
     client.disconnect()
     client.loop_stop()
 
 if __name__ == '__main__':
     run()
+   
